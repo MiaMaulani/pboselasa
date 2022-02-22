ad213b63d22c52b4e9611ee6cfe032d82430e485
MiaMaulani <93563115+MiaMaulani@users.noreply.github.com>
2022-02-22 10:42:57 +0700

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import VueSupabase from "vue-supabase";
+
+Vue.use(VueSupabase, {
+  supabaseUrl: "https://uwqfwhnqklgbrotoexeg.supabase.co",
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNjQzMDc2MzcxLCJleHAiOjE5NTg2NTIzNzF9.IfDp6GZDd7mluz6YPH-bS_s0nX8_odg_WIx_43kjw3U",
+  supabaseOptions: {},
+});
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
